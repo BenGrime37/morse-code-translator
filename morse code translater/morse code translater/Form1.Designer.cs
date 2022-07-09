605f7c3af2558b1aabe41a7a6c41ae26fe436eee
BenGrime37 <108684658+BenGrime37@users.noreply.github.com>
2022-07-09 21:09:40 +0100

    Add files via upload

---
+++ morse code translater/morse code translater/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace morse_code_translater
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.messageBox = new System.Windows.Forms.TextBox();
+            this.Translate = new System.Windows.Forms.Button();
+            this.morseCodeBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lengthLabel = new System.Windows.Forms.Label();
+            this.morselengthLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // messageBox
+            // 
+            this.messageBox.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.messageBox.Location = new System.Drawing.Point(33, 59);
+            this.messageBox.Name = "messageBox";
+            this.messageBox.Size = new System.Drawing.Size(735, 51);
+            this.messageBox.TabIndex = 0;
+            // 
+            // Translate
+            // 
+            this.Translate.Location = new System.Drawing.Point(129, 311);
+            this.Translate.Name = "Translate";
+            this.Translate.Size = new System.Drawing.Size(514, 114);
+            this.Translate.TabIndex = 1;
+            this.Translate.Text = "Translate";
+            this.Translate.UseVisualStyleBackColor = true;
+            this.Translate.Click += new System.EventHandler(this.Translate_Click);
+            // 
+            // morseCodeBox
+            // 
+            this.morseCodeBox.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.morseCodeBox.Location = new System.Drawing.Point(33, 230);
+            this.morseCodeBox.Name = "morseCodeBox";
+            this.morseCodeBox.Size = new System.Drawing.Size(735, 51);
+            this.morseCodeBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Message in here";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Code in here";
+            // 
+            // lengthLabel
+            // 
+            this.lengthLabel.AutoSize = true;
+            this.lengthLabel.Location = new System.Drawing.Point(341, 145);
+            this.lengthLabel.Name = "lengthLabel";
+            this.lengthLabel.Size = new System.Drawing.Size(29, 20);
+            this.lengthLabel.TabIndex = 2;
+            this.lengthLabel.Text = "AA";
+            this.lengthLabel.Visible = false;
+            // 
+            // morselengthLabel
+            // 
+            this.morselengthLabel.AutoSize = true;
+            this.morselengthLabel.Location = new System.Drawing.Point(410, 145);
+            this.morselengthLabel.Name = "morselengthLabel";
+            this.morselengthLabel.Size = new System.Drawing.Size(27, 20);
+            this.morselengthLabel.TabIndex = 2;
+            this.morselengthLabel.Text = "bb";
+            this.morselengthLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.morselengthLabel);
+            this.Controls.Add(this.lengthLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.morseCodeBox);
+            this.Controls.Add(this.Translate);
+            this.Controls.Add(this.messageBox);
+            this.Name = "Form1";
+            this.Text = "Morse Code Translator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox messageBox;
+        private System.Windows.Forms.Button Translate;
+        private System.Windows.Forms.TextBox morseCodeBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lengthLabel;
+        private System.Windows.Forms.Label morselengthLabel;
+    }
+}
+
---
+++ morse code translater/morse code translater/Form1.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace morse_code_translater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Translate_Click(object sender, EventArgs e)
+        {
+            if (messageBox.Text == "" && morseCodeBox.Text == "")//nothing been entered
+            {
+                MessageBox.Show("Must enter the morse code OR a Message");
+            }
+            else if (messageBox.Text != "" && morseCodeBox.Text == "")//text was entered
+            {
+                string messageText = messageBox.Text;
+
+                lengthLabel.Text = messageText.Length.ToString();
+
+                for (int i = 0; i < messageText.Length; i++)
+                {
+
+                    if (messageText[i] == Char.Parse("A") || messageText[i] == Char.Parse("a"))
+                    {
+                        morseCodeBox.AppendText(".-");
+                        morseCodeBox.AppendText(" ");
+                    }
+                    else if (messageText[i] == Char.Parse("B") || messageText[i] == Char.Parse("b"))
+                    {
+                        morseCodeBox.AppendText("-...");
+                        morseCodeBox.AppendText(" ");
+                    }
+
+                    else if (messageText[i] == Char.Parse("C") || messageText[i] == Char.Parse("c"))
+                    {
+                        morseCodeBox.AppendText("-.-.");
+                        morseCodeBox.AppendText(" ");
+                    }
+                    else if (messageText[i] == Char.Parse("D") || messageText[i] == Char.Parse("d"))
+                    {
+                        morseCodeBox.AppendText("-..");
+                        morseCodeBox.AppendText(" ");
+                    }
+
+                    else if (messageText[i] == Char.Parse("E") || messageText[i] == Char.Parse("e"))
+                    {
+                        morseCodeBox.AppendText(".");
+                        morseCodeBox.AppendText(" ");
+                    }
+                    else if (messageText[i] == Char.Parse("F") || messageText[i] == Char.Parse("f"))
+                    {
+                        morseCodeBox.AppendText("..-.");
+                        morseCodeBox.AppendText(" ");
+                    }
+
+                    else if (messageText[i] == Char.Parse("G") || messageText[i] == Char.Parse("g"))
+                    {
+                        morseCodeBox.AppendText("--.");
+                        morseCodeBox.AppendText(" ");
+                    }
+                    else if (messageText[i] == Char.Parse("H") || messageText[i] == Char.Parse("h"))
+                    {
+                        morseCodeBox.AppendText("....");
+                        morseCodeBox.AppendText(" ");
+                    }
+                    else if (messageText[i] == Char.Parse("I") || messageText[i] == Char.Parse("i"))
+                    {
+                        morseCodeBox.AppendText("..");
+                        morseCodeBox.AppendText(" ");
+                    }
+                    else if (messageText[i] == Char.Parse("J") || messageText[i] == Char.Parse("j"))
+                    {
+                        morseCodeBox.AppendText(".---");
+                        morseCodeBox.AppendText(" ");
+                    }
+
+                    else if (messageText[i] == Char.Parse("K") || messageText[i] == Char.Parse("k"))
+                    {
+                        morseCodeBox.AppendText("-.-");
+                        morseCodeBox.AppendText(" ");
+                    }
+                    else if (messageText[i] == Char.Parse("L") || messageText[i] == Char.Parse("l"))
+                    {
+                        morseCodeBox.AppendText(".-..");
+                        morseCodeBox.AppendText(" ");
+                    }
+
+                    else if (messageText[i] == Char.Parse("M") || messageText[i] == Char.Parse("m"))
+                    {
+                        morseCodeBox.AppendText("--");
+                        morseCodeBox.AppendText(" ");
+                    }
+                    else if (messageText[i] == Char.Parse("N") || messageText[i] == Char.Parse("n"))
+                    {
+                        morseCodeBox.AppendText("-.");
+                        morseCodeBox.AppendText(" ");
+                    }
+
+                    else if (messageText[i] == Char.Parse("O") || messageText[i] == Char.Parse("o"))
+                    {
+                        morseCodeBox.AppendText("---");
+                        morseCodeBox.AppendText(" ");
+                    }
+                    else if (messageText[i] == Char.Parse("P") || messageText[i] == Char.Parse("p"))
+                    {
+                        morseCodeBox.AppendText(".--.");
+                        morseCodeBox.AppendText(" ");
+                    }
+                    else if (messageText[i] == Char.Parse("Q") || messageText[i] == Char.Parse("q"))
+                    {
+                        morseCodeBox.AppendText("--.-");
+                        morseCodeBox.AppendText(" ");
+                    }
+
+                    else if (messageText[i] == Char.Parse("R") || messageText[i] == Char.Parse("r"))
+                    {
+                        morseCodeBox.AppendText(".-.");
+                        morseCodeBox.AppendText(" ");
+                    }
+                    else if (messageText[i] == Char.Parse("S") || messageText[i] == Char.Parse("s"))
+                    {
+                        morseCodeBox.AppendText("...");
+                        morseCodeBox.AppendText(" ");
+                    }
+
+                    else if (messageText[i] == Char.Parse("T") || messageText[i] == Char.Parse("t"))
+                    {
+                        morseCodeBox.AppendText("-");
+                        morseCodeBox.AppendText(" ");
+                    }
+                    else if (messageText[i] == Char.Parse("U") || messageText[i] == Char.Parse("u"))
+                    {
+                        morseCodeBox.AppendText("..-");
+                        morseCodeBox.AppendText(" ");
+                    }
+
+                    else if (messageText[i] == Char.Parse("V") || messageText[i] == Char.Parse("v"))
+                    {
+                        morseCodeBox.AppendText("...-");
+                        morseCodeBox.AppendText(" ");
+                    }
+                    else if (messageText[i] == Char.Parse("W") || messageText[i] == Char.Parse("w"))
+                    {
+                        morseCodeBox.AppendText(".--");
+                        morseCodeBox.AppendText(" ");
+                    }
+                    else if (messageText[i] == Char.Parse("X") || messageText[i] == Char.Parse("x"))
+                    {
+                        morseCodeBox.AppendText("-..-");
+                        morseCodeBox.AppendText(" ");
+                    }
+                    else if (messageText[i] == Char.Parse("Y") || messageText[i] == Char.Parse("y"))
+                    {
+                        morseCodeBox.AppendText("-.--");
+                        morseCodeBox.AppendText(" ");
+                    }
+
+                    else if (messageText[i] == Char.Parse("Z") || messageText[i] == Char.Parse("z"))
+                    {
+                        morseCodeBox.AppendText("--..");
+                        morseCodeBox.AppendText(" ");
+                    }
+                    else if (messageText[i] == Char.Parse(" ") || messageText[i] == Char.Parse(" "))
+                    {
+                        morseCodeBox.AppendText(" ");
+                        morseCodeBox.AppendText(" ");
+                    }
+                }
+
+                MessageBox.Show("COMPLETED!");
+            }
+            else if (messageBox.Text == "" && morseCodeBox.Text != "")//morse code to text
+            {
+                string morseCodeText = morseCodeBox.Text;
+                
+
+                int countSpaces = morseCodeBox.Text.Count(Char.IsWhiteSpace); 
+
+
+                string[] code = morseCodeText.Split(" ");
+
+                for (int j = 0; j < countSpaces + 1; j++)
+                {
+                    if (code[j] == ".-")
+                    {
+                        messageBox.AppendText("A");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == "-...")
+                    {
+                        messageBox.AppendText("B");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == "-.-.")
+                    {
+                        messageBox.AppendText("C");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == "-..")
+                    {
+                        messageBox.AppendText("D");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == ".")
+                    {
+                        messageBox.AppendText("E");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == "..-.")
+                    {
+                        messageBox.AppendText("F");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == "--.")
+                    {
+                        messageBox.AppendText("G");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == "....")
+                    {
+                        messageBox.AppendText("H");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == "..")
+                    {
+                        messageBox.AppendText("I");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == ".---")
+                    {
+                        messageBox.AppendText("J");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == "-.-")
+                    {
+                        messageBox.AppendText("K");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == ".-..")
+                    {
+                        messageBox.AppendText("L");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == "--")
+                    {
+                        messageBox.AppendText("M");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == "-.")
+                    {
+                        messageBox.AppendText("N");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == "---")
+                    {
+                        messageBox.AppendText("O");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == ".--.")
+                    {
+                        messageBox.AppendText("P");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == "--.-")
+                    {
+                        messageBox.AppendText("Q");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == ".-.")
+                    {
+                        messageBox.AppendText("R");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == "...")
+                    {
+                        messageBox.AppendText("S");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == "-")
+                    {
+                        messageBox.AppendText("T");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == "..-")
+                    {
+                        messageBox.AppendText("U");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == "...-")
+                    {
+                        messageBox.AppendText("V");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == ".--")
+                    {
+                        messageBox.AppendText("W");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == "-..-")
+                    {
+                        messageBox.AppendText("X");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == "-.--")
+                    {
+                        messageBox.AppendText("Y");
+                        messageBox.AppendText(" ");
+                    }
+                    else if (code[j] == "--..")
+                    {
+                        messageBox.AppendText("Z");
+                        messageBox.AppendText(" ");
+                    }
+
+                }
+                MessageBox.Show("COMPLETED!");
+            }
+        }
+
+    }
+}
